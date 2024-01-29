66fc4aea8fcc225655a4e99d02af3de97d38a435
Sandeep <sandeeppakho55@gmail.com>
2024-01-29 11:21:26 +0530

    FEAT: archive functionality

--- app/(main)/_components/Item.tsx
+++ app/(main)/_components/Item.tsx
@@ -1,10 +1,25 @@
 "use client";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 import { Skeleton } from "@/components/ui/skeleton";
 import { api } from "@/convex/_generated/api";
 import { Id } from "@/convex/_generated/dataModel";
 import { cn } from "@/lib/utils";
+import { useUser } from "@clerk/clerk-react";
 import { useMutation } from "convex/react";
-import { ChevronDown, ChevronRight, LucideIcon, Plus } from "lucide-react";
+import {
+  ChevronDown,
+  ChevronRight,
+  LucideIcon,
+  MoreHorizontal,
+  Plus,
+  Trash,
+} from "lucide-react";
 import { useRouter } from "next/navigation";
 import React from "react";
 import { toast } from "sonner";
@@ -33,27 +48,45 @@ export const Item = ({
   onExpand,
   expanded,
 }: ItemProps) => {
+  const user = useUser();
   const create = useMutation(api.documents.create);
-  const router = useRouter()
+  const archive = useMutation(api.documents.archive);
+  const router = useRouter();
 
-  const handleExpand = (event:React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+  const onArchive = (event: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+    event.stopPropagation();
+    if (!id) return;
+    const promise = archive({ id: id as Id<"documents"> });
+    toast.promise(promise, {
+      loading: "Archiving document...",
+      success: "Document archived!",
+      error: "Failed to archive document",
+    });
+  };
+
+  const handleExpand = (
+    event: React.MouseEvent<HTMLDivElement, MouseEvent>
+  ) => {
     event.stopPropagation();
     onExpand?.();
-  }
+  };
 
-  const onCreate= (event:React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+  const onCreate = (event: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
     event.stopPropagation();
-    if(!id) return;
-    const promise = create({ title: "New Document", parentDocument: id}).then((documentId) => {
-      if(!expanded) onExpand?.();
+    if (!id) return;
+    const promise = create({
+      title: "New Document",
+      parentDocument: id as Id<"documents">,
+    }).then((documentId) => {
+      if (!expanded) onExpand?.();
       // router.push(`/documents/${documentId}`);
     });
     toast.promise(promise, {
       loading: "Creating document...",
       success: "Document created!",
       error: "Failed to create document",
-    })
-  }
+    });
+  };
 
   const ChevronIcon = expanded ? ChevronDown : ChevronRight;
   return (
@@ -62,7 +95,7 @@ export const Item = ({
         onclick();
       }}
       role="button"
-      style={{ paddingLeft: level ? `${(level * 12) + 12}px` : "12px" }}
+      style={{ paddingLeft: level ? `${level * 12 + 12}px` : "12px" }}
       className={cn(
         " group min-h-[27px] text-sm py-1 pr-3 w-full  hover:bg-primary/5 flex items-center text-muted-foreground font-medium",
         active && "bg-primary/15"
@@ -86,37 +119,67 @@ export const Item = ({
 
       <span className=" truncate">{label}</span>
       {isSearch && (
-        <kbd className=" ml-auto pointer-events-none inline-flex h-5 select-none items-center gap-1 rounded border bg-muted px-1.5 
-        font-mono text-[10px] font-medium text-muted-foreground opacity-100">
+        <kbd
+          className=" ml-auto pointer-events-none inline-flex h-5 select-none items-center gap-1 rounded border bg-muted px-1.5 
+        font-mono text-[10px] font-medium text-muted-foreground opacity-100"
+        >
           <span className=" text-xs ">⌘</span>K
         </kbd>
       )}
-      {
-        !!id && (
-          <div className=" ml-auto flex items-center gap-x-2">
-            <div
+      {!!id && (
+        <div className=" ml-auto flex items-center gap-x-2">
+          <DropdownMenu>
+            <DropdownMenuTrigger onClick={(e) => e.stopPropagation()} asChild>
+              <div
+                role="button"
+                className=" opacity-0 group-hover:opacity-100 h-full ml-auto rounded-sm hover:bg-neutral-300 dark:bg-neutral-600"
+              >
+                <MoreHorizontal className=" h-4 w-4 text-muted-foreground" />
+              </div>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent
+              className=" w-60"
+              align="start"
+              side="right"
+              forceMount
+            >
+              <DropdownMenuItem
+                onClick={(e) => {
+                  onArchive(e);
+                }}
+              >
+                <Trash className=" h-4 w-4 mr-2 inline-block" />
+                Delete
+              </DropdownMenuItem>
+              <DropdownMenuSeparator />
+              <div className="text-xs text-muted-foreground p-2">
+                Las edited by: {user?.user?.fullName}
+              </div>
+            </DropdownMenuContent>
+          </DropdownMenu>
+          <div
             role="button"
             onClick={onCreate}
-            className=" opacity-0 group-hover:opacity-100 h-full ml-auto rounded-sm hover:bg-neutral-300 dark:bg-neutral-600">
-              <Plus className=" h-4 w-4 text-muted-foreground" />
-            </div>
+            className=" opacity-0 group-hover:opacity-100 h-full ml-auto rounded-sm hover:bg-neutral-300 dark:bg-neutral-600"
+          >
+            <Plus className=" h-4 w-4 text-muted-foreground" />
           </div>
-        )
-      }
+        </div>
+      )}
     </div>
   );
 };
 
-Item.Skeleton = function ItemSkeleton({level}:{level?:number}) {
-  return(
+Item.Skeleton = function ItemSkeleton({ level }: { level?: number }) {
+  return (
     <div
       style={{
-        paddingLeft: level ? `${(level * 12) + 25}` : "12px",
+        paddingLeft: level ? `${level * 12 + 25}` : "12px",
       }}
       className=" flex gap-x-2 py-[3px]"
     >
-      <Skeleton className=" h-4 w-4"/>
-      <Skeleton className=" h-4 w-[30%]"/>
+      <Skeleton className=" h-4 w-4" />
+      <Skeleton className=" h-4 w-[30%]" />
     </div>
-  )
-}
+  );
+};
