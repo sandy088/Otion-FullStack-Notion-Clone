042bf7f731b4b75632bbaf160d34855771e414d8
Sandeep <sandeeppakho55@gmail.com>
2024-02-03 12:05:29 +0530

    Added: Realtime Document toolbar

--- app/(main)/(routes)/documents/[documentId]/page.tsx
+++ app/(main)/(routes)/documents/[documentId]/page.tsx
@@ -1,8 +1,29 @@
-import React from 'react'
+"use client"
+import { Toolbar } from "@/components/Toolbar"
+import { api } from "@/convex/_generated/api"
+import { Id } from "@/convex/_generated/dataModel"
+import { useQuery } from "convex/react"
 
-const DocumentIdPage = ({params}:{params:{documentId:string}}) => {
+interface DocumentIdPageProps {
+  params: {
+    documentId: Id<'documents'>
+  }
+
+}
+const DocumentIdPage = ({params}:DocumentIdPageProps) => {
+  const document = useQuery(api.documents.getById,{documentId: params.documentId})
+  
+  if(document === undefined) return <div>Loading...</div>
+  if(document === null) return <div>Document not found</div>
+  
   return (
-    <div>page:-{params.documentId}</div>
+    <div className=" pb-40">
+      <div className=" h-[35vh]"/>
+      <div className=" md:max-w-3xl lg:max-w-4xl mx-auto">
+        <Toolbar initialData={document}/>
+      </div>
+
+    </div>
   )
 }
 
