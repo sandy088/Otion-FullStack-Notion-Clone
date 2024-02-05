35136261f3e86df98aeb11ff6a5a060cb431b45a
Sandeep <sandeeppakho55@gmail.com>
2024-02-05 10:35:02 +0530

    FIX: Minor

--- hooks/use-cover-image.tsx
+++ hooks/use-cover-image.tsx
@@ -1,15 +1,18 @@
 import { create } from "zustand";
 
 type coverImageStore = {
+    url?: string;
     isOpen: boolean;
     setIsOpen: () => void;
     onClose: () => void;
+    onReplace: (url: string) => void;
 }
 
 export const useCoverImage = create<coverImageStore>((set)=>{
     return {
         isOpen: false,
-        setIsOpen: () => set({ isOpen: true }),
-        onClose: () => set({ isOpen: false }),
+        setIsOpen: () => set({ isOpen: true, url: undefined}),
+        onClose: () => set({ isOpen: false, url: undefined}),
+        onReplace: (url: string) => set({isOpen:true, url })
     }
 })
